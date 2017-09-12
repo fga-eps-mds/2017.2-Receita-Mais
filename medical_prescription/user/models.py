ab12f677efc3220297979464e18b4affcfb7322c
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2017-09-11 22:05:04 -0300

    Changing user model class name

--- medical_prescription/user/models.py
+++ medical_prescription/user/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Mongo(models.Model):
+class User (models.Model):
     name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
