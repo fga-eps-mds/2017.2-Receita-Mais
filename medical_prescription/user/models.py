6b6f7039411307a168115fc02a48380a968e6793
Mateus Nóbrega <mateusn@outlook.com>
2017-09-12 22:26:32 -0300

    #1 - Creating User and HealthProfessional model structures.
    Signed-off-by: Mateus Nóbrega <mateusn@outlook.com>
    Signed-off-by: Guilherme Marques <guilherme.marques.moreira@gmail.com>

--- medical_prescription/user/models.py
+++ medical_prescription/user/models.py
@@ -1,7 +1,25 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
 
-class User (models.Model):
-    name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
+class User(AbstractBaseUser):
+    first_name = models.CharField(blank=False, max_length=50)
+    last_name = models.CharField(blank=False, max_length=50)
+    date_of_birth = models.DateField()
+    phone = models.CharField(max_length=11, blank=True)
+    email = models.EmailField(unique=True)
+    sex = models.CharField(max_length=10)
+
+    is_active = models.BooleanField(default=False)
+
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
+
+    def get_short_name(self):
+        return self.first_name
+
+
+class HealthProfessional(models.Model):
+    user = models.OneToOneField(User)
+    crm = models.CharField(max_length=10)
+    crm_state = models.CharField(max_length=2)
