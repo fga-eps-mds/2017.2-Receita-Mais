e1510919f78bcc0e18ce48fd31fcfb2b2e01f9d3
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2017-09-16 20:12:50 -0300

    #1 - Creating templates, views and urls
    Signed-off-by: GuiMarques98 <guilherme.marques.moreira@gmail.com>
    Mateus Nóbrega <mateusn@outlook.com>

--- medical_prescription/user/urls.py
+++ medical_prescription/user/urls.py
@@ -1,5 +1,10 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 
+from . import views
 
 urlpatterns = (
+    url(r'^register/$', views.register_health_professional),
+    url(r'^view/$', views.view_health_professional),
+    url(r'^edit/$', views.edit_health_professional),
+    url(r'^delete/$', views.delete_health_professional),
 )
--- medical_prescription/user/views.py
+++ medical_prescription/user/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def register_health_professional(request):
+    return render(request, 'register_health_professional.html')
+
+
+def view_health_professional(request):
+    return render(request, 'view_health_professional.html')
+
+
+def edit_health_professional(request):
+    return render(request, 'edit_health_professional.html')
+
+
+def delete_health_professional(request):
+    return render(request, 'delete_health_professional.html')
