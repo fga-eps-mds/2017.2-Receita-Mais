986449ed0f2dcf83dc43bf15238bc0889b2308d2
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2017-09-17 22:14:09 -0300

    #1 - Creating validations from health professional.
    Signed-off-by: GuiMarques98 <guilherme.marques.moreira@gmail.com>
    Signed-off-by: Michel Camargo <michel.dcamargo@gmail.com>
    Signed-off-by: Mateus Nóbrega <mateusn@outlook.com>

--- medical_prescription/user/forms.py
+++ medical_prescription/user/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import HealthProfessional, User
+from django.core.exceptions import ValidationError
 # from datetime import date
 
 
@@ -10,11 +11,37 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = (
-            'name', 'email', 'password', 'date_of_birth', 'phone', 'sex')
+        fields = [
+            'name', 'email', 'password', 'date_of_birth', 'phone', 'sex'
+            ]
 
     def clean(self):
-        pass
+        name = self.cleaned_data.get('name')
+        phone = self.cleaned_data.get('phone')
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+        password_confirmation = self.cleaned_data.get('confirm_password')
+
+        email_from_database = User.objects.filter(email=email)
+
+        if email_from_database.exists():
+            raise ValidationError("Email ja cadastrado!")
+        if len(password) < 6:
+            raise forms.ValidationError("Senha tem que ter mais de 6 caracteres!")
+        if len(password) > 12:
+            raise forms.ValidationError("Senha tem que ter menos de 12 caracteres!")
+        if password != password_confirmation:
+            raise forms.ValidationError("As senhas não coicidem")
+        if len(email) > 50:
+            raise forms.ValidationError("Email deve conter menos de 50 caracteres!")
+        if len(email) < 5:
+            raise forms.ValidationError("Email deve conter mais de 5 caracteres!")
+        if len(name) > 50:
+            raise forms.ValidationError("Nome deve conter menos de 50 caracteres!")
+        if len(name) < 5:
+            raise forms.ValidationError("Nome deve conter mais de 5 caracteres!")
+        if len(phone) > 11:
+            raise forms.ValidationError("Telefone deve conter menos de 11 caracteres!")
 
 
 class HealthProfessionalForm(forms.ModelForm):
@@ -23,4 +50,15 @@ class HealthProfessionalForm(forms.ModelForm):
         fields = ('crm', 'crm_state')
 
     def clean(self):
-        pass
+        crm = self.cleaned_data.get('crm')
+        crm_state = self.cleaned_data.get('crm_state')
+
+        crm_from_database = User.objects.filter(crm=crm)
+        crm_state_from_database = User.objects.filter(crm_state=crm_state)
+
+        if len(crm) != 5:
+            raise forms.ValidationError("O CRM deve conter 5 caracteres!")
+        elif len(crm_state) != 2:
+            raise forms.ValidationError("O estado do crm deve conter 2 caracteres!")
+        elif crm_from_database.exists() and crm_state_from_database.exists():
+            raise forms.ValidationError("CRM ja existe!")
