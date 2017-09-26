5fc3fc3180bb69b694ef202c750d75f9bd2dfc9c
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2017-09-26 19:50:48 -0300

    Updating login tests.
    Signed-off-by: GuiMarques98 <guilherme.marques.moreira@gmail.com>
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>

--- medical_prescription/user/tests.py
+++ medical_prescription/user/tests.py
@@ -1,23 +1,41 @@
-# from django.test import TestCase
-# from django.test.client import RequestFactory
+from django.test import TestCase
 
-# from .models import User
-# from .views import login_view
+from user.models import User
+from user.views import ResetPasswordProfile
 
 # Create your tests here.
 
 
-# class Login_Teste(TestCase):
-#    def setup(self):
-#        self.User = User()
-#        self.user.email = "user@user.com
-#        self.user.password = "testando"
-#        self.factory = RequestFactory(s)
-#        self.user.save()
+class LoginTeste(TestCase):
+    def setUp(self):
+        self.user = User()
+        self.user.name = "Felipe"
+        self.user.date_of_birth = "1995-01-01"
+        self.user.phone = "9999-9999"
+        self.user.sex = "F"
+        self.user.email = "user@user.com"
+        self.user.password = "testando"
+        self.user.save()
 
-#    def test(self):
-#        request = self.factory.get('home/login')
-#        request.user = self.user
-#        response = login_view(request)
+    def test_save_name(self):
+        self.assertEqual(self.user.name, "Felipe")
 
-#        self.assertEqual(response.status_code, 200)
+    def test_save_date_of_birth(self):
+        self.assertEqual(self.user.date_of_birth, "1995-01-01")
+
+    def test_save_phone(self):
+        self.assertEqual(self.user.phone, "9999-9999")
+
+    def test_save_sex(self):
+        self.assertEqual(self.user.sex, "F")
+
+    def test_save_email(self):
+        self.assertEqual(self.user.email, "user@user.com")
+
+    def test_save_password(self):
+        self.assertEqual(self.user.password, "testando")
+
+    def test_get_short_name(self):
+        user_name = User.objects.get(name="Felipe")
+        print("AQUI" + user_name.name)
+        self.assertEqual(user_name.get_short_name(), "Felipe")
