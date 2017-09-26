c9d407b9cab23cc1db832268f79cabeb03b074b5
Unknown <lucas.hh@hotmail.com>
2017-09-26 15:39:07 -0300

    #7 - Update Forms Tests

--- medical_prescription/user/tests.py
+++ medical_prescription/user/tests.py
@@ -1,23 +1,150 @@
-# from django.test import TestCase
-# from django.test.client import RequestFactory
+from django.test import TestCase
+from django.test.client import RequestFactory
 
-# from .models import User
-# from .views import login_view
+import datetime
 
+from user.forms import PatientForm
 # Create your tests here.
 
 
-# class Login_Teste(TestCase):
-#    def setup(self):
-#        self.User = User()
-#        self.user.email = "user@user.com
-#        self.user.password = "testando"
-#        self.factory = RequestFactory(s)
-#        self.user.save()
+class MyTests(TestCase):
 
-#    def test(self):
-#        request = self.factory.get('home/login')
-#        request.user = self.user
-#        response = login_view(request)
+    def setUp(self):
+        self.name_valid = 'Teste Nome'
+        self.name_invalid = 'a'
 
-#        self.assertEqual(response.status_code, 200)
+        self.date_of_birth_valid = '10/12/1990'
+        self.date_of_birth_invalid = '18'
+
+        self.phone_valid = '123456789'
+        self.phone_invalid = '456'
+
+        self.email_valid = 'admin@admin.com'
+        self.email_invalid = 'admin.com'
+
+        self.sex_valid = 'M'
+        self.sex_invalid = 'A'
+
+        self.crm_valid = '12345'
+        self.crm_invalid = '1'
+
+        self.crm_state_valid = 'DF'
+        self.crm_state_invalid = 'asd'
+
+        self.id_document_valid = '12345678910'
+        self.id_document_invalid = '1234'
+
+        self.password_valid = '1234567'
+        self.password_invalid = '1234567891011'
+
+    def test_forms_patient_is_valid(self):
+        form_data = {'name': self.name_valid,
+                     'phone': self.phone_valid,
+                     'email': self.email_valid,
+                     'password': self.password_valid,
+                     'confirm_password': self.password_valid,
+                     'id_document': self.id_document_valid,
+                     'sex': self.sex_valid,
+                     'date_of_birth': self.date_of_birth_valid}
+        form = PatientForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_forms_patient_name_is_not_valid(self):
+        form_data = {'name': self.name_invalid,
+                     'phone': self.phone_valid,
+                     'email': self.email_valid,
+                     'password': self.password_valid,
+                     'confirm_password': self.password_valid,
+                     'id_document': self.id_document_valid,
+                     'sex': self.sex_valid,
+                     'date_of_birth': self.date_of_birth_valid}
+        form = PatientForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_forms_patient_phone_is_not_valid(self):
+        form_data = {'name': self.name_valid,
+                     'phone': self.phone_invalid,
+                     'email': self.email_valid,
+                     'password': self.password_valid,
+                     'password_confirmation': self.password_valid,
+                     'id_document': self.id_document_valid,
+                     'sex': self.sex_valid,
+                     'date_of_birth': self.date_of_birth_valid}
+        form = PatientForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_forms_patient_email_is_not_valid(self):
+        form_data = {'name': self.name_valid,
+                     'phone': self.phone_valid,
+                     'email': self.email_invalid,
+                     'password': self.password_valid,
+                     'confirm_password': self.password_valid,
+                     'id_document': self.id_document_valid,
+                     'sex': self.sex_valid,
+                     'date_of_birth': self.date_of_birth_valid}
+        form = PatientForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
+    def test_forms_patient_password_is_not_valid(self):
+        form_data = {'name': self.name_valid,
+                     'phone': self.phone_valid,
+                     'email': self.email_valid,
+                     'password': self.password_invalid,
+                     'confirm_password': self.password_valid,
+                     'id_document': self.id_document_valid,
+                     'sex': self.sex_valid,
+                     'date_of_birth': self.date_of_birth_valid}
+        form = PatientForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
+    def test_forms_patient_password_confirmation_is_not_valid(self):
+        form_data = {'name': self.name_valid,
+                     'phone': self.phone_valid,
+                     'email': self.email_valid,
+                     'password': self.password_valid,
+                     'confirm_password': self.password_invalid,
+                     'id_document': self.id_document_valid,
+                     'sex': self.sex_valid,
+                     'date_of_birth': self.date_of_birth_valid}
+        form = PatientForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
+    def test_forms_id_document_is_not_valid(self):
+        form_data = {'name': self.name_valid,
+                     'phone': self.phone_valid,
+                     'email': self.email_valid,
+                     'password': self.password_valid,
+                     'confirm_password': self.password_valid,
+                     'id_document': self.id_document_invalid,
+                     'sex': self.sex_valid,
+                     'date_of_birth': self.date_of_birth_valid}
+        form = PatientForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
+    def test_forms_patient_date_of_birth_is_not_valid(self):
+        form_data = {'name': self.name_valid,
+                     'phone': self.phone_valid,
+                     'email': self.email_valid,
+                     'password': self.password_valid,
+                     'confirm_password': self.password_valid,
+                     'id_document': self.id_document_valid,
+                     'sex': self.sex_valid,
+                     'date_of_birth': self.date_of_birth_invalid}
+        form = PatientForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_forms_patient_sex_is_not_valid(self):
+        form_data = {'name': self.name_valid,
+                     'phone': self.phone_valid,
+                     'email': self.email_valid,
+                     'password': self.password_valid,
+                     'confirm_password': self.password_valid,
+                     'id_document': self.id_document_valid,
+                     'sex': self.sex_invalid,
+                     'date_of_birth': self.date_of_birth_valid}
+        form = PatientForm(data=form_data)
+        self.assertFalse(form.is_valid())
