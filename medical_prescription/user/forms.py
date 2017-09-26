96be1e44d9f9c3be2949666ef1e45de5f86ada57
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2017-09-26 15:39:21 -0300

    Adding login page template

--- medical_prescription/user/forms.py
+++ medical_prescription/user/forms.py
@@ -13,8 +13,10 @@ class UserLoginForm(forms.Form):
     Login Form.
     '''
 
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput, label=('password'))
+    email = forms.EmailField(widget = forms.TextInput(attrs={'class': 'form-control s-form-v3__input',
+                                                             'placeholder': 'Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control s-form-v3__input',
+                                                                 'placeholder': 'Senha'}))
 
 
 class FormattedDateField(forms.DateField):
