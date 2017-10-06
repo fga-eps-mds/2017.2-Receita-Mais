ba3f9deb837287e3155be9b7563afdcd0c631181
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2017-10-06 20:53:40 -0300

    #11 - Fixing update patient and health professional.
    Signed-off-by: Lucas Hiroshi <lucas.hh@hotmail.com>

--- medical_prescription/user/views/updatehealthprofessional.py
+++ medical_prescription/user/views/updatehealthprofessional.py
@@ -10,5 +10,7 @@ from user.forms import UpdateUserForm
 class UpdateHealthProfessional(UpdateView):
     model = User
     form_class = UpdateUserForm
-    success_url = reverse_lazy('view')
     template_name = 'edit_health_professional.html'
+
+    def get_success_url(self, **kwargs):
+            return reverse_lazy('edit', kwargs={'pk': self.object.id})
