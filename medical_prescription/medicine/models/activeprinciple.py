b16e0ea794404440ac82c56d4407bd20c43543fa
Thiago Nogueira <thiagonf10@gmail.com>
2017-10-10 18:07:07 -0300

    # 38 - Adding fixtures and fixing models in django admin.
    Signed-off-by: lucaskishima <lucaskishima2@gmail.com>
    Signed-off-by: Thiago Nogueira <thiagonf10@gmail.com>

--- medical_prescription/medicine/models/activeprinciple.py
+++ medical_prescription/medicine/models/activeprinciple.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class ActivePrinciple(models.Model):
-    name = models.CharField(min_lenght=5, max_lenght=100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
