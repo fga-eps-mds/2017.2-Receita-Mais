719e6603094eae0cdaac8071ee7e65268b4e13fc
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2017-10-10 19:39:13 -0300

    #25 - Adding fixture and fixing model in django admin.
    Signed-off-by: GuiMarques98 <guilherme.marques.moreira@gmail.com>
    Signed-off-by: Lucas Hiroshi <lucas.hh@hotmail.com>

--- parser/exam.sql
+++ parser/exam.sql
@@ -1,1 +1,7 @@
-\copy exam FROM 'TUSS.txt' DELIMITER ';' CSV;
+/*
+Target Server         : Postgres
+Target Table          : Exam
+
+Date: 2017-10-10
+*/
+\copy exam(id_tuss, description) FROM 'TUSS.txt' DELIMITER ',' CSV;
