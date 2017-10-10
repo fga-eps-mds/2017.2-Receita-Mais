6ede38480c9eec44fb9150a9b0c3008514faa571
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2017-10-09 21:15:03 -0300

    #25 - Creating models.
    Signed-off-by: Lucas Hiroshi <lucas.hh@hotmail.com>
    Signed-off-by: Guilherme Marques <guilherme.marques.moreira@gmail.com>

--- medical_prescription/exam/models/customexam.py
+++ medical_prescription/exam/models/customexam.py
@@ -2,10 +2,8 @@
 from django.db import models
 
 # Local Django
-from exam import constants
 from .exam import Exam
 
 
 class CustomExam(Exam):
-    id_tuss = models.CharField(max_length=constants.ID_TUSS_MAX_LENGTH, blank=True)
-    description = models.CharField(max_length=constants.DESC_MAX_LENGTH)
+    health_professional_FK = models.ForeignKey("user.HealthProfessional", on_delete=models.CASCADE)
