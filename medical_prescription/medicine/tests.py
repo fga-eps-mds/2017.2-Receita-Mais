b75f122541847d0b5f67b392b1b220658ebdf517
Thiago Nogueira <thiagonf10@gmail.com>
2017-10-11 15:27:03 -0300

    #39 - Adding test medicine.
    Signed-off-by: Lucas Kishima <lucaskishima2@gmail.com>
    Signed-off-by: Thiago Nogueira <thiagonf10@gmail.com>

--- medical_prescription/medicine/tests.py
+++ medical_prescription/medicine/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ListActivePrincipleViewTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/medicine/list/')
+
+    def test_get_active_principle(self):
+        self.assertTrue('list_active_principle' in self.resp.context)
