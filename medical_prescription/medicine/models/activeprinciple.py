9e7e29d28af4c48a156755186a46a6e9239575ba
lucaskishima <lucaskishima2@gmail.com>
2017-10-11 17:45:37 -0300

    #40 - Creating initial structure of custom active principle. Signed-off-by: thiagonf <thiagonf10@gmail.com> Signed-off-by: lucaskishima <lucaskishima2@gmail.com>

--- medical_prescription/medicine/models/activeprinciple.py
+++ medical_prescription/medicine/models/activeprinciple.py
@@ -1,5 +1,10 @@
 from django.db import models
+from user.models import HealthProfessional
 
 
 class ActivePrinciple(models.Model):
     name = models.CharField(max_length=100)
+
+
+class CustomActivePrinciple(ActivePrinciple):
+    created_by = models.ForeignKey('user.HealthProfessional')
