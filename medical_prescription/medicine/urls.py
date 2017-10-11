db9bdd5a2b3a98730c3449df63efba9b8c294652
Thiago Nogueira <thiagonf10@gmail.com>
2017-10-10 23:05:36 -0300

    # 39 - Fixing urls active principle.
    Signed-off-by: Lucas Kishima <lucaskishima2@gmail.com>
    Signed-off-by: Thiago Nogueira <thiagonf10@gmail.com>

--- medical_prescription/medicine/urls.py
+++ medical_prescription/medicine/urls.py
@@ -1,4 +1,4 @@
 from django.conf.urls import url
 from .views import ListActivePrinciple
 
-url(r'^list/$', ListActivePrinciple.as_view(), name='activePrinciple_list'),
+urlpatterns = (url(r'^list/$', ListActivePrinciple.as_view(), name='activeprinciple_list'),)
--- medical_prescription/medicine/views.py
+++ medical_prescription/medicine/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
 from .models import ActivePrinciple
+from django.views.generic import ListView
 
 
 class ListActivePrinciple(ListView):
     model = ActivePrinciple
-    template_name = "list_medicine.html"
+    template_name = 'list_medicine.html'
     context_object_name = 'list_active_principle'
     paginate_by = 20
 
