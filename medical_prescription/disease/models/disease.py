3380330b0d55e3f527e2a58c9c476244c3dc2dcd
Michel Camargo <michel.dcamargo@gmail.com>
2017-10-11 22:14:15 -0300

    #35 - Fixing identation.
    Signed-off-by: Michel Camargo <michel.dcamargo@gmail.com>
    Signed-off-by: Marcelo Augusto <mekmay@hotmail.com>

--- medical_prescription/disease/models/disease.py
+++ medical_prescription/disease/models/disease.py
@@ -8,9 +8,9 @@ from disease import constants
 # Model that defines diseases.
 class Disease(models.Model):
 	# Disease fields.
-	id_cid_10 = models.CharField(max_length=constants.ID_CID_MAX_LENGTH,
-                                 unique=True)
-    description = models.CharField(max_length=constants.DESC_CID_MAX_LENGTH)
+	id_cid_10 = models.CharField(max_length=constants.ID_CID_MAX_LENGTH, unique=True)
+	description = models.CharField(max_length=constants.DESC_CID_MAX_LENGTH)
 
-    def __str__(self):
-        return self.description
+	def __str__(self):
+		return self.description
+		
