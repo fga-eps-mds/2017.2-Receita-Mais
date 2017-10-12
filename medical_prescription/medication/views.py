9e012d40cc501b97470266709a2f129cd34f3fcc
Felipe Borges <bumbleblo2013@gmail.com>
2017-10-12 13:20:58 -0300

    #34 - Create model list and template.
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>
    Signed-off-by: Mateus Nóbrega <mateusn@outlook.com>

--- medical_prescription/medication/views.py
+++ medical_prescription/medication/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .model import Medication
+from django.views.generic import ListView
 
-# Create your views here.
+
+class ListMedication(ListView):
+
+    '''
+    Query and list objects Medication.
+    '''
+
+    template_name = 'list_medication.html'
+    model = Medication
+
+    # Get 20 queries of objects Medication.
+    def get_20_query_set(self, request):
+        return self.model.objects.all()[:20]
