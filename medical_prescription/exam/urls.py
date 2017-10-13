f57b96c1f1ec556f77780aa2ca7a4e55d6cdfc2d
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2017-10-13 01:52:21 -0300

    #28 - Updating views, urls, templates.
    Signed-off-by: Lucas Hiroshi <lucas.hh@hotmail.com>
    Signed-off-by: GuiMarques98 <guilherme.marques.moreira@gmail.com>

--- medical_prescription/exam/urls.py
+++ medical_prescription/exam/urls.py
@@ -4,10 +4,12 @@ from django.conf.urls import url
 # Local Django
 
 from .views import (ListExams,
+                    ListCustomExams,
                     CreateCustomExamsView)
 
 
 urlpatterns = (
     url(r'^list_exams/$', ListExams.as_view(), name='list_exams'),
+    url(r'^list_custom_exams/$', ListCustomExams.as_view(), name='list_custom_exams'),
     url(r'^create_custom_exams/$', CreateCustomExamsView.as_view(), name='create_custom_exams'),
 )
--- medical_prescription/exam/views/__init__.py
+++ medical_prescription/exam/views/__init__.py
@@ -1,2 +1,3 @@
 from .list_exams import ListExams
+from .list_custom_exams import ListCustomExams
 from .createcustomexam import CreateCustomExamsView
