42a68c6f34f6cc02b5502cdde00011a08448fdc0
Natalia Rodrigues <nataliamrq@gmail.com>
2017-10-13 18:30:53 -0300

    #24 - Applying edit password templates.
    Signed-off-by: Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
    Signed-off-by: Natalia Rodrigues <nataliamrq@gmail.com>

--- medical_prescription/user/views/updatepasswordview.py
+++ medical_prescription/user/views/updatepasswordview.py
@@ -1,7 +1,6 @@
 # Django
 from django.shortcuts import render, redirect
 from django.views.generic.edit import UpdateView
-from django.contrib.auth.models import User
 
 # Local Django
 from user.forms import UpdatePasswordForm
@@ -11,7 +10,6 @@ class UpdateUserPassword(UpdateView):
 
     def edit_password_view(request, email):
         form = UpdatePasswordForm(user=request.user, data=request.POST or None)
-        print("Renderizando password")
 
         if request.method == "POST":
             print("Solicitado")
@@ -21,15 +19,13 @@ class UpdateUserPassword(UpdateView):
                 print(user.name)
                 user.set_password(password)
                 user.save()
-                print("password atualizado")
 
                 return redirect('/')
             else:
-                print("form invalid")
                 # Nothing to do.
                 pass
         else:
             # Nothing to do.
             pass
 
-        return render(request, 'editpassword.html', {'form': form})
+        return render(request, 'edit_health_professional_password.html', {'form': form})
