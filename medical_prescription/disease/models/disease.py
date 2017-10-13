99f5afbcf647d6b89ab63978f4b77dfc96f321da
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2017-10-13 02:02:35 -0300

    #28 - Adding tests.
    Signed-off-by: Lucas Hiroshi <lucas.hh@hotmail.com>
    Signed-off-by: GuiMarques98 <guilherme.marques.moreira@gmail.com>

--- medical_prescription/disease/models/disease.py
+++ medical_prescription/disease/models/disease.py
@@ -7,10 +7,10 @@ from disease import constants
 
 # Model that defines diseases.
 class Disease(models.Model):
-	# Disease fields.
-	id_cid_10 = models.CharField(max_length=constants.ID_CID_MAX_LENGTH, unique=True)
-	description = models.CharField(max_length=constants.DESC_CID_MAX_LENGTH)
 
-	def __str__(self):
-		return self.description
-		
+    # Disease fields.
+    id_cid_10 = models.CharField(max_length=constants.ID_CID_MAX_LENGTH, unique=True)
+    description = models.CharField(max_length=constants.DESC_CID_MAX_LENGTH)
+
+    def __str__(self):
+        return self.description
