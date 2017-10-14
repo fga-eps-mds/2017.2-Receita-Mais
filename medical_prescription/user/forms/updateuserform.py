d3891f46a5edaa496361419572d4e602204e2650
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2017-10-14 00:29:15 -0300

    #30 - Updating UpdateUserForm.
    Signed-off-by: GuiMarques98 <guilherme.marques.moreira@gmail.com>
    Signed-off-by: Lucas Hiroshi <lucas.hh@hotmail.com>

--- medical_prescription/user/forms/updateuserform.py
+++ medical_prescription/user/forms/updateuserform.py
@@ -1,13 +1,20 @@
 # standard library
 from datetime import date
+import logging
 
 # django
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 # local django
 from user.forms import FormattedDateField
 from user.models import User
 from user.validators import UserValidator
+from user import constants
+
+# Set level logger.
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(constants.DEFAULT_LOGGER)
 
 
 class UpdateUserForm(forms.ModelForm):
@@ -29,6 +36,7 @@ class UpdateUserForm(forms.ModelForm):
         """
         Get user fields.
         """
+        logger.debug("Start clean data in UpdateUserForm.")
 
         name = self.cleaned_data.get('name')
         phone = self.cleaned_data.get('phone')
@@ -36,20 +44,30 @@ class UpdateUserForm(forms.ModelForm):
         date_of_birth = self.cleaned_data.get('date_of_birth')
         self.validator_all(name, phone, password, date_of_birth)
 
+        logger.debug("Exit clean data in UpdateUserForm.")
+
     def validator_all(self, name, phone, password, date_of_birth):
         """
         Checks validator in all fields.
         """
 
+        logger.debug("Start validations in UpdateUserForm.")
+
+        self.verify_password(password)
+
         validator = UserValidator()
-        validator.validator_password(password, password)
         validator.validator_name(name)
+        validator.validator_password(password, password)
         validator.validator_phone_number(phone)
         validator.validator_date_of_birth(date_of_birth)
+
+        logger.debug("Exit validations in UpdateUserForm.")
 
     def verify_password(self, password):
         """
         Verifies if the given password matches the one in the database.
         """
+        check_password = self.instance.check_password(password)
 
-        return self.instance.check_password(password)
+        if not check_password:
+            raise forms.ValidationError({'password': [_(constants.PASSWORD_MATCH)]})
