c46c10ccb8654a743ae6c2d5fe25990a21686db2
Mateus Nóbrega <mateusn@outlook.com>
2017-10-14 13:58:59 -0300

    #32 - Adding medication form tests.
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>
    Signed-off-by: Mateus Nóbrega <mateusn@outlook.com>

--- medical_prescription/medication/test/test_list_medication_by_heath_professional.py
+++ medical_prescription/medication/test/test_list_medication_by_heath_professional.py
@@ -1,4 +1,6 @@
-from django.test import TestCase, RequestFactory
+from django.test import TestCase
+from django.test.client import RequestFactory, Client
+
 from user.models import HealthProfessional
 from medication.models import Medication
 from medication.views import ListMedicationByHealthProfessional
@@ -7,22 +9,22 @@ from medication.views import ListMedicationByHealthProfessional
 class TestMedicationList(TestCase):
 
     def setUp(self):
+        self.my_view = ListMedicationByHealthProfessional()
+        self.factory = RequestFactory()
 
         # Making a HealthProfessional
         self.user = HealthProfessional()
+        self.user.pk = 1
+        self.user.email = "test@test.com"
+        self.user.password = "test404"
         self.user.crm = "54321"
         self.user.save()
 
-        self.view = ListMedicationByHealthProfessional
-
-        # Making medication
         self.medication = Medication()
         self.medication.name = "Medicamento Teste"
         self.medication.active_ingredient = "Teste Lab"
         self.medication.health_professional = self.user
         self.medication.save()
-
-        self.factory = RequestFactory()
 
         self.health_professional_medications = Medication.objects.filter(health_professional=self.user)
 
@@ -32,8 +34,6 @@ class TestMedicationList(TestCase):
     def teste_len_false(self):
         self.assertNotEqual(len(self.health_professional_medications), 50)
 
-    def test_list(self):
-            request = self.factory
-            request.user = self.user
-            list_medications = self.view.get_queryset(self.view)
-            self.assertEqual(self.health_professional_medications, list_medications)
+    '''def teste_get(self):
+        self.client.login(email='test@test.com', password='test404')
+        self.assertTrue('list_medications' in self.resp.context)'''
