9eaf363608d44801e3b8a53a66f40107a0f6cf3e
Natalia Rodrigues <nataliamrq@gmail.com>
2017-10-13 21:53:12 -0300

    #24 - Adding comments to the update password form and view.
    Signed-off-by: Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
    Signed-off-by: Natalia Rodrigues <nataliamrq@gmail.com>

--- medical_prescription/user/forms/updatepasswordform.py
+++ medical_prescription/user/forms/updatepasswordform.py
@@ -3,10 +3,11 @@ from django import forms
 from user.validators import UserValidator
 
 
+# This class is responsible for defining the existing fields in the
+# password-editing form and for validating such fields.
 class UpdatePasswordForm(forms.Form):
-    """
-    Form to confirm password.
-    """
+
+    # Form fields to edit password.
     old_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Senha Atual', 'class': 'form-control'}),
                                    label='')
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Nova Senha', 'class': 'form-control'}),
@@ -19,16 +20,21 @@ class UpdatePasswordForm(forms.Form):
         self.user = user
         super(UpdatePasswordForm, self).__init__(data=data)
 
+    # This method makes the vaidations necessary for the completed form to be valid.
     def clean(self, *args, **kwargs):
         old_password = self.cleaned_data.get('old_password')
         password = self.cleaned_data.get('password')
         password_confirmation = self.cleaned_data.get('password_confirmation')
 
+        # Checking if the old password field matches with the one in database.
         if not self.user.check_password(old_password):
             raise forms.ValidationError('Senha Invalida')
+
+        # Checking if the password confirmation field matches with the password field.
         elif(password != password_confirmation):
             raise forms.ValidationError('As senhas devem ser iguais')
 
+        # Calling the 'Validator' Class to make the new password validation.
         validator = UserValidator()
         validator.validator_password(password, password_confirmation)
 
--- medical_prescription/user/views/updatepasswordview.py
+++ medical_prescription/user/views/updatepasswordview.py
@@ -6,11 +6,16 @@ from django.views.generic.edit import UpdateView
 from user.forms import UpdatePasswordForm
 
 
+# This class is responsible for making the user password change.
 class UpdateUserPassword(UpdateView):
 
+    # This method is responsible for making the patient password change.
     def edit_patient_password_view(request, email):
+
+        # Getting the corresponding form to make the password change.
         form = UpdatePasswordForm(user=request.user, data=request.POST or None)
 
+        # If the form is completed and valid, the patient password is changed.
         if request.method == "POST":
             if form.is_valid():
                 password = form.cleaned_data.get('new_password')
@@ -29,9 +34,13 @@ class UpdateUserPassword(UpdateView):
 
         return render(request, 'edit_patient_password.html', {'form': form})
 
+    # This method is responsible for making the health professional password change.
     def edit_health_professional_password_view(request, email):
+
+        # Getting the corresponding form to make the password change.
         form = UpdatePasswordForm(user=request.user, data=request.POST or None)
 
+        # If the form is completed and valid, the health professional password is changed.
         if request.method == "POST":
             print("Solicitado")
             if form.is_valid():
