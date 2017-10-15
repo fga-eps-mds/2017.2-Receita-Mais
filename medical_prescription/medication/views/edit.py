4a820b1525d61989acbb339339e54603f63466cb
Felipe Borges <bumbleblo2013@gmail.com>
2017-10-15 17:49:28 -0200

    #33 - Fixing redirect.
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>
    Signed-off-by: Mateus Nóbrega <mateusn@outlook.com>

--- medical_prescription/medication/views/edit.py
+++ medical_prescription/medication/views/edit.py
@@ -1,6 +1,7 @@
 from django.views.generic import UpdateView
 from medication.models import Medication
 from medication.forms import EditForm
+from django.urls import reverse
 
 
 class UpdateMedication(UpdateView):
@@ -8,8 +9,10 @@ class UpdateMedication(UpdateView):
         Edit Medication attributes with this class.
     '''
 
+    sucess_url = 'list_medication'
     model = Medication
     template_name = 'edit_medication.html'
     form_class = EditForm
 
-    sucess_url = 'list_all_medications.html'
+    def get_success_url(self):
+        return reverse(self.sucess_url)
