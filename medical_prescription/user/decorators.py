2d1ca81342c021680d507a0e691cc4f3c0dd4fb0
Marcelo Augusto <mekmay@hotmail.com>
2017-10-17 21:22:10 -0200

    #64 - Adding decorator to verify if user is owner of edit request.

--- medical_prescription/user/decorators.py
+++ medical_prescription/user/decorators.py
@@ -15,3 +15,32 @@ def is_health_professional(method):
             return redirect('/')
 
     return wrap
+
+
+def is_patient(method):
+    """
+    Verify if user is a patient.
+    """
+    def wrap(request, *args, **kwargs):
+        is_patient = hasattr(request.user, 'patient')
+        if is_patient:
+            return method(request, *args, **kwargs)
+        else:
+            return redirect('/')
+
+    return wrap
+
+
+def health_professional_is_account_owner(method):
+    """
+    Verify if health professional is a owner of request
+    """
+    def wrap(request, *args, **kwargs):
+        is_health_professional = hasattr(request.user, 'healthprofessional')
+        is_owner = int(request.user.pk) == int(kwargs.get('pk'))
+        if is_owner and is_health_professional:
+            return method(request, *args, **kwargs)
+        else:
+            return redirect('/')
+
+    return wrap
