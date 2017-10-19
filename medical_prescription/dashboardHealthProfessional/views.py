dc8789e970076a525f6b6e66c2d8a251f41d1996
Marcelo Augusto <mekmay@hotmail.com>
2017-10-18 22:10:22 -0200

    #64 - Adding decorators to verifying if user is health professional in views of dashboards.
    Signed-off-by: lucaskishima <lucaskishima2@gmail.com>
    Signed-off-by: Marcelo Augusto <mekmay@hotmail.com>

--- medical_prescription/dashboardHealthProfessional/views.py
+++ medical_prescription/dashboardHealthProfessional/views.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# Django imports
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+# Local Django imports
+from user.decorators import is_health_professional
 
-@login_required()
+
+@login_required
+@is_health_professional
 def home(request):
-
     return render(request, 'healthprofessional.html')
