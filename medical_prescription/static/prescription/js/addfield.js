be9bb8927ea0e6053e55737eae5a23d700900512
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-10-22 02:36:15 -0200

    #66 - Customizing autocomplete in fields.

--- medical_prescription/static/prescription/js/addfield.js
+++
@@ -1,9 +0,0 @@
-var counter = 1;
-
-var limit = 3;
-
-function addInput(divName, input_name){
-        var $div = $('input[id^="'+ divName +'"]:last');
-        var $klon = $div.clone(true).attr('id', "#"+divName).insertAfter("#"+divName);
-        counter++;
-}
--- medical_prescription/static/prescription/js/autocompleteexam.js
+++ medical_prescription/static/prescription/js/autocompleteexam.js
@@ -1,3 +1,22 @@
+$.ui.autocomplete.prototype._renderItem = function(ul, item) {
+  item.label = item.label.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + $.ui.autocomplete.escapeRegex(this.term) + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<strong>$1</strong>");
+
+  $img = $('<img>');
+  $img.attr({
+    src: 'https://mt.googleapis.com/vt/icon/name=icons/onion/73-hospitals.png',
+    alt: item.label
+  });
+
+  var description = '<font size="2" color="gray">' + item.description + '</font>';
+
+  return $("<li></li>")
+    .data("item.autocomplete", item)
+    .append($img)
+    .append("<a>" + item.label + "</a>")
+    .append(": <br>" + description + "")
+    .appendTo(ul);
+};
+
 $('.exam-field').focus(function() {
   console.log(this)
   $(this).autocomplete({
@@ -9,10 +28,14 @@ $('.exam-field').focus(function() {
           'search': request.term
         },
         success: function(data) {
-					console.log(data.list)
+          console.log(data.list);
           response(data.list);
         }
       });
+    },
+    select: function(event, ui) {
+      console.log(ui)
+      $(this).prop("readonly", true);
     },
     minLength: 2
   });
