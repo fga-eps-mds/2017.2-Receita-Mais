b51c663a4099867eb171cf41240dd340163b0d7b
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2017-10-23 22:13:38 -0200

    #78 - Adding autocomplete to DefaultExam and linking nav bar.
    Signed-off-by: GuiMarques98 <guilherme.marques.moreira@gmail.com>

--- medical_prescription/prescription/views/autocompleteexam.py
+++ medical_prescription/prescription/views/autocompleteexam.py
@@ -3,7 +3,7 @@ import json
 from django.views.generic import View
 from django.http import HttpResponse
 
-from exam.models import CustomExam
+from exam.models import DefaultExam, CustomExam
 
 
 class AutoCompleteExam(View):
@@ -15,17 +15,17 @@ class AutoCompleteExam(View):
             search = request.GET.get('term', '')
 
             # TODO(Ronyell) Change logic to all exam.
-            queryset = CustomExam.objects.filter(name__icontains=search)[:5]
+            queryset = DefaultExam.objects.filter(description__icontains=search)[:5]
             list_exam = []
 
             # TODO(Ronyell) Change the required data.
             # Encapsulates in a json needed to be sent.
             for exam in queryset:
                 exam_item = {}
-                exam_item['value'] = exam.name
+                exam_item['value'] = exam.description
                 exam_item['id'] = exam.auto_increment_id
                 exam_item['description'] = exam.description
-                exam_item['name'] = exam.name
+                exam_item['name'] = exam.id_tuss
 
                 list_exam.append(exam_item)
             data = json.dumps(list_exam)
