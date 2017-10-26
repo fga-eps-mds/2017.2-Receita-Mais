7db1699d039b034a25a377a03ce8e830847b4fce
Lucas Kishima <lucaskishima2@gmail.com>
2017-10-26 03:48:08 -0200

    #64 - Testing edit custom active principle view.
    Signed-off-by: Lucas Kishima <lucaskishima2@gmail.com>
    Signed-off-by: Marcelo Augusto <mekmay@hotmail.com>

--- medical_prescription/medicine/test/test_edit_custom_principle_views.py
+++ medical_prescription/medicine/test/test_edit_custom_principle_views.py
@@ -1,46 +1,75 @@
+# Django imports
 from django.test import TestCase
-from django.test.client import RequestFactory, Client
+from django.test.client import RequestFactory
+from django.contrib.auth.models import AnonymousUser
 
+# Local Django imports
 from medicine.views import EditCustomActivePrinciple
-from medicine.forms import CustomActivePrincipleForm
 from medicine.models import CustomActivePrinciple
-from user.models import HealthProfessional
+from user.models import HealthProfessional, Patient
 
 
-class TestEditCustom(TestCase):
+class EditCustomActivePrincipleTest(TestCase):
     """
-    Testing methods of Class CreateCustomExamsView.
+    Testing methods of Class EditCustomActivePrinciple.
     """
 
     def setUp(self):
-        self.my_view = CustomActivePrincipleForm()
-        self.name_valid = "Alguma coisa"
-        self.name_exists = "Invalido"
+        self.factory = RequestFactory()
+        self.health_professional = HealthProfessional.objects.create_user(email='doctor@doctor.com', password='senha12')
+        self.patient = Patient.objects.create_user(email='patient@patient.com', password='senha12')
 
-        self.name_max = """aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                                aaaaaaaaaaaaaaaaaaaaaaaa+1"""
+        self.description = "Examina alguma coisa"
+        self.name = "Alguma coisa"
 
         custom_principle = CustomActivePrinciple()
-        custom_principle.name = "Teste"
-        custom_principle.pk = "1"
-        user = HealthProfessional()
-        user.crm = "54321"
-        user.save()
-        custom_principle.created_by = user
+        custom_principle.created_by = self.health_professional
+        custom_principle.pk = 1
         custom_principle.save()
 
-        self.factory = RequestFactory()
-        self.my_view = EditCustomActivePrinciple()
-        self.client = Client()
+    # Testing view calls
+    def test_get_without_login(self):
+        request = self.factory.get('medicine/edit/(?P<pk>[0-9]+)/')
+        request.user = AnonymousUser()
 
-    # Testing method 'post'.
-    def test_post(self):
-        response = self.client.post('/medicine/edit/1/',
-                                    {'name': self.name_valid})
+        response = EditCustomActivePrinciple.as_view()(request, pk=1)
         self.assertEqual(response.status_code, 302)
 
-    # Testing method 'post'.
-    def test_post_invalid(self):
-        response = self.client.post('/medicine/edit/2/',
-                                    {'name': self.name_valid})
+    def test_get_with_patient(self):
+        request = self.factory.get('medicine/edit/(?P<pk>[0-9]+)/')
+        request.user = self.patient
+
+        response = EditCustomActivePrinciple.as_view()(request, pk=1)
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_with_health_professional(self):
+        request = self.factory.get('medicine/edit/(?P<pk>[0-9]+)/')
+        request.user = self.health_professional
+
+        response = EditCustomActivePrinciple.as_view()(request, pk=1)
+        self.assertEqual(response.status_code, 200)
+
+    # Testing post
+    def test_post_without_login(self):
+        request = self.factory.post('medicine/edit/(?P<pk>[0-9]+)/',
+                                    {'name': self.name})
+        request.user = AnonymousUser()
+
+        response = EditCustomActivePrinciple.as_view()(request, pk=1)
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_with_patient(self):
+        request = self.factory.post('medicine/edit/(?P<pk>[0-9]+)/',
+                                    {'name': self.name})
+        request.user = self.patient
+
+        response = EditCustomActivePrinciple.as_view()(request, pk=1)
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_with_health_professional(self):
+        request = self.factory.post('medicine/edit/(?P<pk>[0-9]+)/',
+                                    {'name': self.name})
+        request.user = self.health_professional
+
+        response = EditCustomActivePrinciple.as_view()(request, pk=1)
         self.assertEqual(response.status_code, 302)
