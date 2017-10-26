ebe44660e31d564b15ed3b6b44b4c48f9fd48508
Thiago Nogueira <thiagonf10@gmail.com>
2017-10-26 13:34:30 -0200

    #83 - Adding new fields pattient.

--- medical_prescription/user/models/patient.py
+++ medical_prescription/user/models/patient.py
@@ -1,6 +1,6 @@
 # Django
 from django.db import models
-
+from django.core.validators import MaxValueValidator
 # Local Django
 from .user import User
 from .usermanager import UserManager
@@ -8,5 +8,9 @@ from .usermanager import UserManager
 
 class Patient(User):
     id_document = models.CharField(blank=False, max_length=32, default='')
-
+    CEP = models.PositiveIntegerField(blank=False, validators=[MaxValueValidator(99999999)])
+    UF = models.CharField(blank=False, max_length=2, default='')
+    city = models.CharField(blank=False, max_length=50, default='')
+    neighborhood = models.CharField(blank=False, max_length=50, default='')
+    complement = models.CharField(blank=False, max_length=200, default='')
     objects = UserManager()
