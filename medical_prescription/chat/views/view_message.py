464a2dc1bdf5452bb80350e5aefc24040a457f3d
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2017-10-27 17:56:29 -0200

    #81 - Adding response in view.
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>
    Signed-off-by: Lucas Hiroshi <lucas.hh@hotmail.com>

--- medical_prescription/chat/views/view_message.py
+++ medical_prescription/chat/views/view_message.py
@@ -1,12 +1,17 @@
+
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import FormMixin
+from django.shortcuts import render, redirect
+from datetime import date
 
-from chat.models import Message
+from chat.models import Message, Response
+from chat.forms import CreateMessage
 
 
-class MessageDetailView(DetailView):
+class MessageDetailView(DetailView, FormMixin):
 
-    model = Message
     template_name = "view_message.html"
+    form_class = CreateMessage
 
     context_object_name = 'list'
     model = Message
@@ -14,3 +19,28 @@ class MessageDetailView(DetailView):
 
     def get_queryset(self):
         return self.model.objects.filter(user_to=self.request.user)
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            text = form.cleaned_data.get('text')
+
+            response = Response()
+            response.user_from = self.object.user_from
+            response.user_to = self.object.user_to
+            response.text = text
+            response.dat = date.today()
+            response.save()
+
+            self.object.save()
+
+            self.object.messages.add(response)
+
+            return redirect('/dashboard_health_professional/health_professional')
+
+        return render(request, self.template_name, {'form': form})
