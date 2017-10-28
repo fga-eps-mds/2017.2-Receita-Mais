5c12a05e4ec8c7704ccedbf2a166d0e2abb1380f
Thiago Nogueira <thiagonf10@gmail.com>
2017-10-28 10:41:23 -0200

    #83 - Fixing name variables.

--- medical_prescription/static/patient/patient.js
+++ medical_prescription/static/patient/patient.js
@@ -1,62 +1,62 @@
-  $(document).ready(function($) {
+$(document).ready(function($) {
 
-    //Creating mascara for form fields
-    var $date_of_birth = $("#id_date_of_birth");
-    $date_of_birth.mask('00/00/0000');
+  //Creating mascara for form fields
+  var $date_of_birth = $("#id_date_of_birth");
+  $date_of_birth.mask('00/00/0000');
 
-    function limpa_formulário_cep() {
-      // Cleans cep form values.
-      $("#rua").val("");
-      $("#bairro").val("");
-      $("#cidade").val("");
-      $("#id_UF").val("");
-    }
+  function clean_form_cep() {
+    // Cleans cep form values.
+    $("#id_complement").val("");
+    $("#id_neighborhood").val("");
+    $("#id_city").val("");
+    $("#id_UF").val("");
+  }
 
-    //When the cep field loses focus.
-    $("#id_CEP").blur(function() {
+  //When the cep field loses focus.
+  $("#id_CEP").blur(function() {
 
-      //New variable "cep" with digits only.
-      var cep = $(this).val().replace(/\D/g, '');
+    //New variable "cep" with digits only.
+    var cep = $(this).val().replace(/\D/g, '');
 
 
-      if (cep != "") {
+    if (cep != "") {
 
-        //Regular expression to validate the CEP.
-        var validacep = /^[0-9]{8}$/;
+      //Regular expression to validate the CEP.
+      var validationcep = /^[0-9]{8}$/;
 
-        // Validate the format of the CEP.
-        if (validacep.test(cep)) {
+      // Validate the format of the CEP.
+      if (validacep.test(cep)) {
 
-          // Fill fields while querying API.
-          $("#id_complement").val("Buscando...");
-          $("#id_neighborhood").val("Buscando...");
-          $("#id_city").val("Buscando...");
-          $("#id_UF").val("Buscando...");
+        // Fill fields while querying API.
+        $("#id_complement").val("Buscando...");
+        $("#id_neighborhood").val("Buscando...");
+        $("#id_city").val("Buscando...");
+        $("#id_UF").val("Buscando...");
 
-          // Check the webservice viacep.com.br/
-          $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function(dados) {
+        // Check the webservice viacep.com.br/
+        $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function(dados) {
 
-            if (!("erro" in dados)) {
-              // Refreshes fields with query values.
-              $("#id_complement").val(dados.logradouro);
-              $("#id_neighborhood").val(dados.bairro);
-              $("#id_city").val(dados.localidade);
-              $("#id_UF").val(dados.uf);
-            } //end if.
-            else {
-              limpa_formulário_cep();
-              alert("CEP não encontrado.");
-            }
-          });
-        } //end if.
-        else {
-          limpa_formulário_cep();
-          alert("Formato de CEP inválido.");
-        }
+          if (!("erro" in dados)) {
+            // Refreshes fields with query values.
+            $("#id_complement").val(dados.logradouro);
+            $("#id_neighborhood").val(dados.bairro);
+            $("#id_city").val(dados.localidade);
+            $("#id_UF").val(dados.uf);
+          } //end if.
+          else {
+            clean_form_cep();
+            alert("CEP não encontrado.");
+          }
+        });
       } //end if.
       else {
-        // no value, clean form.
-        limpa_formulário_cep();
+        clean_form_cep();
+        alert("Formato de CEP inválido.");
       }
-    });
+    } //end if.
+    else {
+      // no value, clean form.
+      clean_form_cep();
+    }
   });
+});
