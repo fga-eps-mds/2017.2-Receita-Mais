d9d44094bf757352a4efc603ef99b972e8a10077
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2017-11-03 11:44:51 -0200

    #109 - Adding dispatch in recommendation view.
    Signed-off-by: GuiMarques98 <guilherme.marques.moreira@gmail.com>
    Signed-off-by: Thiago Nogueira <thiagonf10@gmail.com>

--- medical_prescription/recommendation/views/createrecomendationcustom.py
+++ medical_prescription/recommendation/views/createrecomendationcustom.py
@@ -1,7 +1,10 @@
 from django.views.generic import FormView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 from recommendation.models import CustomRecommendation
 from recommendation.forms import CreateRecomendationCustomForm
+from user.decorators import is_health_professional
 
 
 class CustomRecommendationCreateView(FormView):
@@ -9,3 +12,8 @@ class CustomRecommendationCreateView(FormView):
     form_class = CreateRecomendationCustomForm
     success_url = 'dashboard_health_professional/health_professional/'
     template_name = 'createcustomrecomendatiom.html'
+
+    @method_decorator(login_required)
+    @method_decorator(is_health_professional)
+    def dispatch(self, *args, **kwargs):
+        return super(CustomRecommendationCreateView, self).dispatch(*args, **kwargs)
