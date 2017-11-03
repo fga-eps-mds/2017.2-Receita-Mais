09279435cfd70646c7e760cb0df8e95535f27f96
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2017-11-03 21:35:01 -0200

    #100 - Updating Models and Templates.
    Signed-off-by: Lucas Hiroshi <lucas.hh@hotmail.com>
    Signed-off-by: Natalia Rodrigues <nataliamrq@gmail.com>

--- medical_prescription/chat/models/response.py
+++ medical_prescription/chat/models/response.py
@@ -1,6 +1,8 @@
 from django.db import models
+
 from chat import constants
 from user.models import User
+from .filetype import CustomFileField
 
 
 class Response(models.Model):
@@ -12,5 +14,6 @@ class Response(models.Model):
     user_to = models.ForeignKey(User, related_name="user_response_to")
 
     text = models.CharField(max_length=constants.MAX_LENGTH_TEXT_MESSAGE)
+    files = CustomFileField(upload_to='media/', content_types=['application/pdf', 'image/png', 'audio/mpeg', ], max_upload_size=20971520, blank=True, null=True)
 
     date = models.DateField(auto_now=True)
