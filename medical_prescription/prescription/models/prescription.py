5debbf158e9594bc591404b6383b080e03a2324b
Natalia Rodrigues <nataliamrq@gmail.com>
2017-11-07 10:49:01 -0200

    #129 - Creating prescription model especializations.
    Signed-off-by: Guilherme Marques <guilherme.marques.moreira@gmail.com>
    Signed-off-by: Natalia Rodrigues <nataliamrq@gmail.com>

--- medical_prescription/prescription/models/prescription.py
+++ medical_prescription/prescription/models/prescription.py
@@ -3,12 +3,10 @@ from django.db import models
 
 # Django Local
 from disease.models import Disease
-from user import constants
 
 
 class Prescription(models.Model):
     """
     Prescription model that contains patient and cid to prescription.
     """
-    patient = models.CharField(max_length=constants.NAME_MAX_LENGHT)
     cid = models.ForeignKey(Disease, null=True, blank=True)
