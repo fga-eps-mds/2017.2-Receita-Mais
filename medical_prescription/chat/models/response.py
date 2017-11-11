1aabc92a2f8d6d9c9fc15570fec4985a539897da
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2017-11-10 22:16:17 -0200

    #100 - Fixing hash.
    Signed-off-by: Lucas Hiroshi <lucas.hh@hotmail.com>
    Signed-off-by: Natalia Rodrigues <nataliamrq@gmail.com>

--- medical_prescription/chat/models/response.py
+++ medical_prescription/chat/models/response.py
@@ -8,21 +8,26 @@ from chat import constants
 from user.models import User
 
 from uuid import uuid4
+from django.utils.deconstruct import deconstructible
 
 
-def path_and_rename(path):
-    def wrapper(instance, filename):
+@deconstructible
+class UploadToPathAndRename(object):
+
+    def __init__(self, path):
+        self.sub_path = path
+
+    def __call__(self, instance, filename):
         ext = filename.split('.')[-1]
         # get filename
         if instance.pk:
             filename = '{}.{}'.format(instance.pk, ext)
         else:
             # set filename as random string
-            hashname = hashlib.sha256(str(random.getrandbits(256)).encode('utf-8')).hexdigest()
             filename = '{}.{}'.format(uuid4().hex, ext)
+            hashname = hashlib.sha256(str(random.getrandbits(256)).encode('utf-8')).hexdigest()
         # return the whole path to the file
-        return os.path.join(path, hashname, filename)
-    return wrapper
+        return os.path.join(self.sub_path, hashname, filename)
 
 
 class Response(models.Model):
@@ -34,6 +39,6 @@ class Response(models.Model):
     user_to = models.ForeignKey(User, related_name="user_response_to")
 
     text = models.CharField(max_length=constants.MAX_LENGTH_TEXT_MESSAGE)
-    files = models.FileField(upload_to=path_and_rename('upload/media/'), blank=True, null=True)
+    files = models.FileField(upload_to=UploadToPathAndRename(os.path.join('upload', 'files')), blank=True, null=True)
 
     date = models.DateField(auto_now=True)
