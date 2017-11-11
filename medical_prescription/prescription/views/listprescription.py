5d0cf042d2613a090872ceb5d13b1782da35be9b
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2017-11-11 11:37:17 -0200

    #130 - Updating templates and view.
    Signed-off-by: Lucas Hiroshi <lucas.hh@hotmail.com>
    Signed-off-by: Michel Camargo <michel.dcamargo@gmail.com>

--- medical_prescription/prescription/views/listprescription.py
+++ medical_prescription/prescription/views/listprescription.py
@@ -5,7 +5,10 @@ from django.utils.decorators import method_decorator
 
 # Local Django
 from user.decorators import is_health_professional
-from prescription.models import PatientPrescription, NoPatientPrescription
+from prescription.models import (PatientPrescription,
+                                 NoPatientPrescription,
+                                 PrescriptionRecommendation,
+                                 PrescriptionHasMedicine)
 
 
 @method_decorator(login_required, name='dispatch')
@@ -15,10 +18,15 @@ class ListPrescription(TemplateView):
         View for list all prescriptions in database.
     '''
     template_name = 'list_prescription.html'
-    # Listing all objects Medication in database.
+    paginate_by = 20
 
+    # Listing all objects related to prescriptions in database.
     def get_context_data(self, **kwargs):
-        return {
-                'list_prescription': PatientPrescription.objects.filter(health_professional=self.request.user),
-                'list_prescription_no_patient': NoPatientPrescription.objects.filter(health_professional=self.request.user),
-               }
+        return{
+              'list_prescription': PatientPrescription.objects.filter(health_professional=self.request.user),
+              'list_prescription_no_patient': NoPatientPrescription.objects.filter(
+                                                                                   health_professional=self.request.user
+                                                                                  ),
+              'list_recommendation': PrescriptionRecommendation.objects.all(),
+              'list_medicine': PrescriptionHasMedicine.objects.all(),
+              }
