6c0d6f0e383510ca79a85405c80e4c75bb6ad097
Felipe Borges <bumbleblo2013@gmail.com>
2017-11-14 15:32:23 -0200

    #162 - Update show prescrition view.
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>
    Signed-off-by: GuiMarques98 <guilherme.marques.moreira@gmail.com>

--- medical_prescription/prescription/views/showprescription.py
+++ medical_prescription/prescription/views/showprescription.py
@@ -2,48 +2,35 @@
 from django.views.generic import DetailView
 from django.template.loader import render_to_string
 from django.http import JsonResponse
-from django.forms import formset_factory
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
 # Local Django
+from prescription.models import (Prescription)
 from user.decorators import is_health_professional
-from prescription.forms import (CreatePrescriptionForm,
-                                MedicinePrescriptionForm,
-                                RecommendationPrescriptionForm,
-                                ExamPrescriptionForm,
-                                )
 
 
-class CreatePrescriptionView(DetailView):
+class ShowDetailPrescriptionView(DetailView):
     """
-    Responsible for rendering to fields.
+    Print done prescription.
     """
-    template_name = 'show_prescription_medicine.html'
-    # Defines that these forms will have multiple instances.
-    MedicinePrescriptionFormSet = formset_factory(MedicinePrescriptionForm)
-    RecommendationPrescriptionFormSet = formset_factory(RecommendationPrescriptionForm)
-    ExamPrescriptionFormSet = formset_factory(ExamPrescriptionForm)
+    template_name = 'show_detail_prescription.html'
 
     @method_decorator(login_required)
     @method_decorator(is_health_professional)
     def dispatch(self, *args, **kwargs):
-        return super(CreatePrescriptionView, self).dispatch(*args, **kwargs)
+        return super(ShowDetailPrescriptionView, self).dispatch(*args, **kwargs)
 
+    # Return a JSON's prescription.
     def get(self, request, *args, **kwargs):
-        """
-        Rendering form in view.
-        """
-        prescription_form = CreatePrescriptionForm(request.GET or None)
-        form_medicine = self.MedicinePrescriptionFormSet(request.GET or None, prefix='form_medicine')
-        form_recommendation = self.RecommendationPrescriptionFormSet(request.GET or None, prefix='form_reccomendation')
-        form_exam = self.ExamPrescriptionFormSet(request.GET or None, prefix='form_exam')
-
         data = dict()
-        context = {'prescription_form': prescription_form,
-                   'form_medicine': form_medicine,
-                   'form_recommendation': form_recommendation,
-                   'form_exam': form_exam}
+        context = self.get_context(request)
         data['html_form'] = render_to_string(self.template_name, context, request=request)
+
         # Json to communication Ajax.
         return JsonResponse(data)
+
+    # Get a Prescription object in database.
+    def get_context(self, request):
+        pk = self.kwargs['pk']
+        return Prescription.objects.get(pk=pk)
