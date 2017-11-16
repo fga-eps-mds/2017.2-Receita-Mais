25529cb4f0b1d91955de61c368bc675861f7c150
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2017-11-16 21:13:10 -0200

    #162 - Adding medicine and manipulated medicine querys in detail prescription.
    Signed-off-by: GuiMarques98 <guilherme.marques.moreira@gmail.com>
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>

--- medical_prescription/prescription/views/showprescription.py
+++ medical_prescription/prescription/views/showprescription.py
@@ -6,7 +6,10 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
 # Local Django
-from prescription.models import (Prescription)
+from prescription.models import (Prescription,
+                                 PrescriptionHasMedicine,
+                                 PrescriptionHasManipulatedMedicine,
+                                 )
 from user.decorators import is_health_professional
 
 
@@ -26,9 +29,17 @@ class ShowDetailPrescriptionView(DetailView):
     def get(self, request, *args, **kwargs):
         data = dict()
         prescription = self.get_context(request)
-        context = {'prescription': prescription}
+        prescription_medicine = self.get_has_medicine(prescription)
+        prescription_manipulated_medicine = self.get_has_manipulated_medicine(prescription)
+        context = {
+            'prescription': prescription,
+            'prescription_medicine': prescription_medicine,
+            'prescription_manipulated_medicine': prescription_manipulated_medicine,
+            }
         data['html_form'] = render_to_string(self.template_name, context, request=request)
-        print(prescription)
+        print(prescription_medicine)
+        for x in prescription_medicine:
+            print(x.via)
         # Json to communication Ajax.
         return JsonResponse(data)
 
@@ -36,3 +47,9 @@ class ShowDetailPrescriptionView(DetailView):
     def get_context(self, request):
         pk = self.kwargs['pk']
         return Prescription.objects.get(pk=pk)
+
+    def get_has_medicine(self, prescription):
+        return PrescriptionHasMedicine.objects.filter(prescription_medicine=prescription)
+
+    def get_has_manipulated_medicine(self, prescription):
+        return PrescriptionHasManipulatedMedicine.objects.filter(prescription_medicine=prescription)
