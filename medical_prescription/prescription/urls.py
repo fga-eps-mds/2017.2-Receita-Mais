75e7833e36cda507062afeb46ad695ccac16f0ae
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2017-11-18 00:53:00 -0200

    #159 - Creating view and template for custom pattern.
    Signed-off-by: Lucas Hiroshi <lucas.hh@hotmail.com>
    Signed-off-by: Mateus Nóbrega <mateusn@outlook.com>

--- medical_prescription/prescription/urls.py
+++ medical_prescription/prescription/urls.py
@@ -9,7 +9,8 @@ from .views import (AutoCompleteExam,
                     AutoCompleteMedicine,
                     CreatePrescriptionView,
                     ListPrescription,
-                    printprescription)
+                    printprescription,
+                    CreatePatternView)
 
 urlpatterns = (
     url(r'^$', OpenPrescriptionView.as_view(), name='create_prescription'),
@@ -20,5 +21,6 @@ urlpatterns = (
         name='autocomplete_medicine'),
     url(r'^create_modal/$', CreatePrescriptionView.as_view(), name='create_modal'),
     url(r'^list_prescription/$', ListPrescription.as_view(), name='list_prescription'),
-    url(r'^print_prescription/(?P<pk>[0-9]+)/$', printprescription.generate_pdf, name='print_prescription')
+    url(r'^print_prescription/(?P<pk>[0-9]+)/$', printprescription.generate_pdf, name='print_prescription'),
+    url(r'^create_prescription_model/$', CreatePatternView.as_view(), name='create_prescription_model'),
     )
