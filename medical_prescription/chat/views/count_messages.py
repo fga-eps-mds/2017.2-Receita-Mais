865800490ea021d58c964bd30e7eeca8590f41f5
Felipe Borges <bumbleblo2013@gmail.com>
2017-11-30 02:59:06 -0200

    #101 - Moving script for static file and fixing bug in list.
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>
    signed-off-by: Thiago Nogueira Freire <thiagonf10@gmail.com>

--- medical_prescription/chat/views/count_messages.py
+++ medical_prescription/chat/views/count_messages.py
@@ -11,21 +11,17 @@ class CountMessagesView(View):
         Return a query of messages unread.
     """
     def post(self, request, *args, **kwargs):
-        print('==========================================================')
 
         if request.is_ajax():
 
-            queryset = Response.objects.filter(user_from=request.user, as_read=False)
+            queryset = Response.objects.filter(user_to=request.user, as_read=False)
 
             query_list = []
 
             for message in queryset:
                 message_item = {}
-                message_item['description'] = message.description
+                message_item['text'] = message.text
                 query_list.append(message_item)
-
-            print(query_list)
-            print('\n\n\n\n')
 
             data = json.dumps(query_list)
 
