1b72c06eca036b1b8c3b194e63cfe81e75619d33
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-12-01 10:34:53 -0200

    #192 - Changing modal for show prescription.
    Signed-off-by: GuiMarques98 <guilherme.marques.moreira@gmail.com>
    Signed-off-by: Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>

--- medical_prescription/static/prescription/js/modalform.js
+++ medical_prescription/static/prescription/js/modalform.js
@@ -1,69 +1,109 @@
-$(function () {
+var modal_show_suggestion = false;
 
-    /* Functions */
-    var modalIsCreated = false;
-
-    function loadForm(event) {
-        var btn = $(this);
-        $(event.data.id_modal).modal({
-          backdrop: 'static',
-          keyboard: false
-        })
-        if (!modalIsCreated){
-        $.ajax({
-            url: btn.attr("data-url"),
-            type: 'get',
-            dataType: 'json',
-            async: true,
-            success: function (data) {
-                $(event.data.id_modal + " .modal-content").html("");
-                $(event.data.id_modal).modal("show");
-                modalIsCreated = false;
-                $(event.data.id_modal + " .modal-content").html(data.html_form);
-            }
-        });
-      }else{
-        $(id_modal).modal("show");
-      }
-    };
-
-    function saveForm(event) {
-        var form = $(this);
-        $.ajax({
-            url: form.attr("action"),
-            data: form.serialize(),
-            type: form.attr("method"),
-            dataType: 'json',
-            async: true,
-            success: function (data) {
-                if (data.form_is_valid) {
-                  modalIsCreated = false;
-                  $(event.data.id_modal).modal("hide");
-                }
-                else {
-                    $(event.data.id_modal).modal("show");
-                    $(event.data.id_modal + " .modal-content").html(data.html_form);
-                }
-            }
-        });
-        return false;
-    };
-
-
-    /* Binding */
-
-    // Create prescription.
-    $(".js-create-prescription").click({id_modal: "#modal-prescription"}, loadForm);
-    $("#modal-prescription").on("submit", ".js-prescription-create-form",{id_modal: "#modal-prescription"}, saveForm);
-
-    // Update prescription.
-    $("#prescription-table").on("click", ".js-update-prescription", {id_modal: "#modal-prescription"}, loadForm);
-    $("#modal-prescription").on("submit", ".js-prescription-update-form", {id_modal: "#modal-prescription"}, saveForm);
-
-    // Delete prescription.
-    $("#prescription-table").on("click", ".js-delete-prescription", {id_modal: "#modal-prescription"}, loadForm);
-    $("#modal-prescription").on("submit", ".js-prescription-delete-form", {id_modal: "#modal-prescription"}, saveForm);
-
-    // Show prescription.
-    $(".js-show-prescription").click({id_modal: "#modal-prescription"}, loadForm);
+// Create prescription.
+$(document).on('click', '.js-create-prescription', function() {
+  loadForm(this, "#modal-prescription");
 });
+
+$(document).on('submit', '.js-prescription-create-form', function() {
+  console.log("()()()()()()()()()()");
+  saveForm(this, "#modal-prescription");
+});
+
+// Update prescription.
+$(document).on('click', '.js-update-prescription', function() {
+  loadForm(this, "#modal-prescription");
+});
+
+$(document).on('submit', '.js-prescription-update-form', function() {
+  saveForm(this, "#modal-prescription");
+});
+
+// Delete prescription.
+$(document).on('click', '.js-delete-prescription', function() {
+  loadForm(this, "#modal-prescription");
+});
+
+$(document).on('submit', '.js-prescription-delete-form', function() {
+  saveForm(this, "#modal-prescription");
+});
+
+// Show prescription.
+$(document).on('click', '.js-show-prescription', function() {
+  modal_show_suggestion = false;
+  loadForm(this, "#modal-view");
+});
+
+// Show suggestion
+$(document).on('click', '.js-show-suggestion', function() {
+  $("#modal-prescription").modal("hide");
+  modal_show_suggestion = true;
+  loadForm(this, "#modal-view");
+});
+
+$(document).on('click', '.btn-return-modal', function() {
+  $("#modal-view").modal("hide");
+  if(modal_show_suggestion){
+    modal_show_suggestion = false;
+    $("#modal-prescription").modal("show");
+  }
+});
+
+$(document).on('click', '.js-create-copy', function() {
+  console.log("(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)");
+  $("#modal-view").modal("hide");
+  loadForm(this, "#modal-prescription");
+});
+
+/* Functions */
+var modalIsCreated = false;
+
+function loadForm(event, id_modal) {
+  var btn = $(event);
+
+  $(id_modal).modal({
+    backdrop: 'static',
+    keyboard: false
+  })
+
+  if (!modalIsCreated) {
+    $.ajax({
+      url: btn.attr("data-url"),
+      type: 'get',
+      dataType: 'json',
+      async: true,
+      success: function(data) {
+        $(id_modal + " .modal-content").html("");
+        $(id_modal).modal("show");
+        modalIsCreated = false;
+        $(id_modal + " .modal-content").html(data.html_form);
+      }
+    });
+  } else {
+    $(id_modal).modal("show");
+  }
+};
+
+function saveForm(event) {
+  console.log("++++++++++++++++++++++++++++++");
+  var form = $(this);
+
+  $.ajax({
+    url: form.attr("action"),
+    data: form.serialize(),
+    type: form.attr("method"),
+    dataType: 'json',
+    async: true,
+    success: function(data) {
+      console.log(data.form_is_valid+"++++++++++++++++++++++++++++++");
+      if (data.form_is_valid) {
+        modalIsCreated = false;
+        $(event.data.id_modal).modal("hide");
+      } else {
+        $(event.data.id_modal).modal("show");
+        $(event.data.id_modal + " .modal-content").html(data.html_form);
+      }
+    }
+  });
+  return false;
+};
