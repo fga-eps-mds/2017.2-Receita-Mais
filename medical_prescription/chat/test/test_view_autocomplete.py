1f15689ed72234f4cd479ecb2cef7b65525c7cd6
Natalia Rodrigues <nataliamrq@gmail.com>
2017-12-01 16:12:33 -0200

    #200 - Fixing autocomplete email.
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>
    Signed-off-by: Natalia Rodrigues <nataliamrq@gmail.com>

--- medical_prescription/chat/test/test_view_autocomplete.py
+++ medical_prescription/chat/test/test_view_autocomplete.py
@@ -12,22 +12,31 @@ class TestAutoComplete(TestCase):
         self.health_professional = HealthProfessional.objects.create_user(email='test@test.com',
                                                                           password='teste')
 
-        self.patient = Patient.objects.create_user(email='testpatient@test.com',
+        self.patient = Patient.objects.create_user(name="Joao da Silva",
+                                                   email='testpatient@test.com',
                                                    password='testepatient')
-                                                   
+
         self.link = AssociatedHealthProfessionalAndPatient(associated_health_professional=self.health_professional,
                                                            associated_patient=self.patient,
                                                            is_active=True)
         self.link.save()
 
-    def test_chat_request_autocomplete_cid_fail(self):
+    def test_chat_request_autocomplete_fail(self):
         request = self.factory.get('/chat/ajax/autocomplete_email/?search=test')
         request.user = self.health_professional
         response = AutoCompleteEmail.as_view()(request)
         self.assertNotEquals(response, HttpResponse)
 
-    def test_chat_request_autocomplete_cid_return_one_disease(self):
+    def test_chat_request_autocomplete_by_email(self):
         request = self.factory.get('/chat/ajax/autocomplete_email/?search=test',
+                                   HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+
+        request.user = self.health_professional
+        response = AutoCompleteEmail.as_view()(request)
+        self.assertEquals(response.status_code, 200)
+
+    def test_chat_request_autocomplete_by_name(self):
+        request = self.factory.get('/chat/ajax/autocomplete_email/?search=Joao',
                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest')
 
         request.user = self.health_professional
--- medical_prescription/chat/views/autocomplete_email.py
+++ medical_prescription/chat/views/autocomplete_email.py
@@ -1,8 +1,10 @@
+import json
+
 from django.views.generic import View
-from django.http import JsonResponse
+from django.http import HttpResponse
 
 from user.models import (AssociatedHealthProfessionalAndPatient,
-                         User)
+                         Patient)
 
 
 class AutoCompleteEmail(View):
@@ -13,16 +15,16 @@ class AutoCompleteEmail(View):
 
         if request.is_ajax():
 
-            search = request.GET.get('search', '')
-            query_email = User.objects.filter(email__icontains=search)[:5]
-            query_name = User.objects.filter(name__icontains=search)[:5]
+            search = request.GET.get('term', '')
+            query_email = Patient.objects.filter(email__icontains=search)[:5]
+            query_name = Patient.objects.filter(name__icontains=search)[:5]
             query_list = []
 
             for patient in query_email:
                 relation = AssociatedHealthProfessionalAndPatient.objects.get(associated_health_professional=request.user,
                                                                               associated_patient=patient,
                                                                               is_active=True)
-                self.create_item(query_list, relation.associated_patient)
+                self.create_item(query_list, patient)
 
             for patient in query_name:
                 relation = AssociatedHealthProfessionalAndPatient.objects.get(associated_health_professional=request.user,
@@ -30,24 +32,23 @@ class AutoCompleteEmail(View):
                                                                               is_active=True)
 
                 if self.check_query_list(query_list, relation.associated_patient) is False:
-                    self.create_item(query_list, relation.associated_patient)
+                    self.create_item(query_list, patient)
 
-            data = {
-                'list': query_list,
-            }
-            return JsonResponse(data)
+            data = json.dumps(query_list)
+            mimetype = 'application/json'
+            return HttpResponse(data, mimetype)
 
     def create_item(self, query_list, patient):
         patient_item = {}
-        patient_item['value'] = patient.name + " - " + patient.email
-        patient_item['email'] = patient.email
+        patient_item['value'] = patient.email
+        patient_item['name'] = patient.name
 
         query_list.append(patient_item)
 
     def check_query_list(self, query_list, patient):
 
         for patient_item in query_list:
-            if patient_item['email'] == patient.email:
+            if patient_item['value'] == patient.email:
                 return True
 
         return False
