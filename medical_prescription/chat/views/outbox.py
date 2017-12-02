fea77379975ad193e07856d43c05164c86e45154
Felipe Borges <bumbleblo2013@gmail.com>
2017-12-02 20:17:24 -0200

    #198 - Adding image in template outbox.
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>
    Signed-off-by: Natália Maria Rodrigues Queiroz <nataliamrq@gmail.com>

--- medical_prescription/chat/views/outbox.py
+++ medical_prescription/chat/views/outbox.py
@@ -3,10 +3,11 @@ from django.views.generic import ListView
 from chat.models import Message
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.shortcuts import render
 
 # Local Django
 from user.decorators import is_health_professional
-
+from user.models import User
 
 @method_decorator(login_required, name='dispatch')
 @method_decorator(is_health_professional, name='dispatch')
@@ -19,6 +20,15 @@ class OutboxView(ListView):
     context_object_name = 'outbox'
     model = Message
     paginate_by = 25
+
+    def get(self, request, *args, **kwargs):
+
+        context = {
+            'outbox': Message.objects.filter(user_to=request.user),
+            'image_profile': User.objects.get(email=request.user.email).image_profile.url
+        }
+
+        return render(request, self.template_name, context)
 
     # Return all send Message for the HealthProfessional.
     def get_queryset(self):
