00af16ccff9d213400f91a126cc264d65cadaa77
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-12-03 13:53:52 -0200

    #237 - Listing last prescriptions in dashboard.

--- medical_prescription/dashboardHealthProfessional/views/views.py
+++ medical_prescription/dashboardHealthProfessional/views/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from datetime import datetime, timedelta
 
 # Django imports
 from django.contrib.auth.decorators import login_required
@@ -7,9 +8,19 @@ from django.shortcuts import render
 
 # Local Django imports
 from user.decorators import is_health_professional
+from prescription.models import Prescription
 
 
 @login_required
 @is_health_professional
 def home(request):
-    return render(request, 'healthprofessional.html')
+    health_professional = request.user.healthprofessional
+    one_week_ago = datetime.today() - timedelta(days=7)
+    prescription_quantity = Prescription.objects.filter(date__gte=one_week_ago,
+                                                        health_professional=health_professional).count()
+
+    last_prescriptions = Prescription.objects.filter(health_professional=health_professional).order_by('-id')[:5]
+
+    print(last_prescriptions)
+    return render(request, 'healthprofessional.html', {'prescription_quantity': prescription_quantity,
+                                                       'last_prescriptions': last_prescriptions})
