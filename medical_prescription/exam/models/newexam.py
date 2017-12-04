3f1fcd8525d237b4232a19c88962dd70466b5668
Lucas Kishima <lucaskishima2@gmail.com>
2017-12-04 01:02:33 -0200

    #197 - Upgrading the number of characteres in exam description
    Signed-off-by: João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
    Signed-off-by: Lucas Kishima <lucaskishima2@gmail.com>

--- medical_prescription/exam/models/newexam.py
+++ medical_prescription/exam/models/newexam.py
@@ -2,7 +2,8 @@
 from django.db import models
 
 # Local Django
+from exam import constants
 
 
 class NewExam(models.Model):
-    exam_description = models.CharField(max_length=100)
+    exam_description = models.CharField(max_length=constants.DESC_TUSS_MAX_LENGTH)
