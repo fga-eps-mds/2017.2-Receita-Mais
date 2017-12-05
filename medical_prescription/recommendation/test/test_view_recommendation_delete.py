f820997ccda974c44c2420df26a09fdde3be4393
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2017-12-05 16:35:14 -0200

    #111 - Adding edit custom recommendation tests.

--- medical_prescription/recommendation/test/test_view_recommendation_delete.py
+++ medical_prescription/recommendation/test/test_view_recommendation_delete.py
@@ -12,21 +12,22 @@ class DeleteRecomendationCustomViewTeste(TestCase):
     def setUp(self):
         self.health_professional_1 = HealthProfessional.objects.create_user(email='doctor1@doctor.com',
                                                                             password='senha12')
-        self.health_professional_2 = HealthProfessional.objects.create_user(email='doctor2@doctor.com',
-                                                                            password='senha12')
+
         self.custom_recommendation = CustomRecommendation()
         self.custom_recommendation.pk = 1
         self.custom_recommendation.name = "Testando"
         self.custom_recommendation.description = "Testa isso direito meu amigo"
-        self.custom_recommendation.health_professional = self.health_professional
+        self.custom_recommendation.health_professional = self.health_professional_1
         self.custom_recommendation.save()
+
         self.view = CustomRecommendationDeleteView()
+        self.view_class = CustomRecommendationDeleteView
         self.factory = RequestFactory()
 
-    def post_custom_recommendation_true(self):
+    def test_post_custom_recommendation_true(self):
         request = self.factory.post('/')
 
-        request.user = self.user
+        request.user = self.health_professional_1
         self.view.request = request
         self.view.object = self.custom_recommendation
 
