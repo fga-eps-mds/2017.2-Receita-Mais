cf33350716098680907f0ed364f76adce94bc217
Felipe Borges <bumbleblo2013@gmail.com>
2017-12-05 02:25:41 -0200

    #253 - Modify template of feeback messages.
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>

--- medical_prescription/static/dashboard/dist/js/message_feedback.js
+++ medical_prescription/static/dashboard/dist/js/message_feedback.js
@@ -16,12 +16,31 @@ function feedback(path){
             var cont = 0;
             for(i in data){
               cont ++;
+
               var element = document.createElement("li");
-              var link = document.createElement("a");
+              var list_from = document.createElement("li");
+              var list_link = document.createElement("li");
+
+              var text_user_from = document.createTextNode('De: '+ data[i]['user_from']);
               var text = document.createTextNode(data[i]['text']);
 
-              link.appendChild(text);
-              element.appendChild(link);
+              var lu = document.createElement("ul");
+
+
+              lu.setAttribute("style","list-style-type: none");
+              lu.setAttribute("class", "container-fluid");
+              list_from.setAttribute("class", "label label-success");
+              list_from.setAttribute("style", "font-size:10px;");
+              list_link.setAttribute("style", "color: gray;padding-left:10px;font-size:15px;");
+
+              element.appendChild(lu);
+              lu.appendChild(list_from);
+              lu.appendChild(list_link);
+
+
+              list_from.appendChild(text_user_from);
+              // list_link.appendChild("<textarea readonly>"+ text +"</textarea>");
+              list_link.appendChild(text);
 
               list_messages.appendChild(element);
 
