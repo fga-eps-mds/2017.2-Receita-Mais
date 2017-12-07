3f376a3001fa820b00d2919a1a6b4e06120daad0
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2017-12-06 22:09:55 -0200

    #238 - Adding templates for dashboard.

--- medical_prescription/dashboardPatient/urls.py
+++ medical_prescription/dashboardPatient/urls.py
@@ -1,9 +1,8 @@
 # Django
 from django.conf.urls import url
-from dashboardPatient.views import (home)
-
+from dashboardPatient.views import (HomePatient,
+                                   )
 
 urlpatterns = (
-    url(r'^patient/$', home, name='dashboard_pt'),
-
+    url(r'^patient/$', HomePatient.as_view(), name='dashboard_patient'),
 )
